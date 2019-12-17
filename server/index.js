3f1508c9d32c54579477f6ee78163017bd65a54b
Robert Fox <robert@foxs.com>
2019-12-17 18:16:38 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use('/api', require('./apiRoutes')) // matches all requests to /api
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
+
+app.use(function(err, req, res, next) {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+const port = process.env.PORT || 3000 // this can be very useful if you deploy to Heroku!
+app.listen(port, function() {
+  console.log('Knock, knock')
+  console.log("Who's there?")
+  console.log(`Your server, listening on port ${port}`)
+})
